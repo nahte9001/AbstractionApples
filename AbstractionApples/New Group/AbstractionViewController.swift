886b47f8bb253aba22a74199fd8bc70eef3f98d1
Ethan Torres <nahte9001@gmail.com>
2018-12-06 08:32:17 -0700

    Renamed, reassigned and constrained viewcontrollers

--- AbstractionApples/New Group/AbstractionViewController.swift
+++ AbstractionApples/New Group/AbstractionViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class AbstractionViewController: UIView {
+class AbstractionViewController: UIPageViewController {
 
     /*
     // Only override draw() if you perform custom drawing.
