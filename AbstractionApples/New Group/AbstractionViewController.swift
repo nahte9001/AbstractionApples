f09a1e5c764efe9ba5700527dfeaffc26bc39361
Ethan Torres <nahte9001@gmail.com>
2018-12-06 07:51:53 -0700

    Added AbstractionViewController

---
+++ AbstractionApples/New Group/AbstractionViewController.swift
@@ -0,0 +1,21 @@
+//
+//  AbstractionViewController.swift
+//  AbstractionApples
+//
+//  Created by Torres, Ethan on 12/6/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+import UIKit
+
+class AbstractionViewController: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
