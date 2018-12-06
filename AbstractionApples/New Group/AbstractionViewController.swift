9d4d5bfc0ebd67db9907b57ff16e7e817a4c164b
Ethan Torres <nahte9001@gmail.com>
2018-12-06 09:40:22 -0700

    Aded code for swiping left and right

--- AbstractionApples/New Group/AbstractionViewController.swift
+++ AbstractionApples/New Group/AbstractionViewController.swift
@@ -8,7 +8,102 @@
 
 import UIKit
 
-class AbstractionViewController: UIPageViewController {
+public class AbstractionViewController: UIPageViewController, UIPageViewControllerDataSource
+{
+    //MARK:- Data Members
+private lazy var orderedAbstractionViews : [UIViewController] =
+{
+    return [
+        self.newAbstractionViewController(abstractionLevel: "Swift"),
+        self.newAbstractionViewController(abstractionLevel: "Block"),
+        self.newAbstractionViewController(abstractionLevel: "ByteCode"),
+        self.newAbstractionViewController(abstractionLevel: "Binary"),
+        self.newAbstractionViewController(abstractionLevel: "LogicalGate"),
+        ]
+    }()
+    
+    //MARK: Helper method to retrieve tthe correct ViewController based on the data member
+    private func newAbstractionViewController(abstractionLevel : String) -> UIViewController
+    {
+        return UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "\(abstractionLevel)ViewController")
+    }
+    
+    //MARK:-Lifecycle methods
+    public override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        dataSource = self
+        
+        if let firstViewController = orderedAbstractionViews.first
+        {
+            setViewControllers([firstViewController],
+                               direction: .forward,
+                               animated: true,
+                               completion: nil)
+        }
+    }
+    
+    //MARK:- DataSource required methods
+    ///Swipe Left
+    public func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController:
+        UIViewController) -> UIViewController?
+    {
+        guard let viewControllerIndex = orderedAbstractionViews.index(of: viewController)
+            else
+        {
+            return nil
+        }
+        
+        let previousIndex = viewControllerIndex - 1
+        
+        guard previousIndex >= 0
+            else
+        {
+            return orderedAbstractionViews.last
+        }
+        
+            guard orderedAbstractionViews.count > previousIndex
+                else
+            {
+                return nil
+            }
+            
+            return orderedAbstractionViews[previousIndex]
+        }
+    
+    //Swipe right
+    public func pageviewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController:
+        UIViewController) -> UIViewController?
+    {
+        guard let viewControllerIndex = orderedAbstractionViews.index(of: viewController)
+            else
+        {
+            return nil
+        }
+        
+        let nextIndex = viewControllerIndex + 1
+        
+        guard nextIndex >= 0 else
+        {
+            return nil
+        }
+        
+        guard nextIndex >=0
+            else
+        {
+            return nil
+        }
+        
+        guard nextIndex < ordered AbstractionViews.count
+            else
+        {
+            return orderedAbstractionViews.first
+        }
+        
+        return orderedAbstractionViews[nextIndex]
+    }
+    
+    }
 
     /*
     // Only override draw() if you perform custom drawing.
@@ -18,4 +113,4 @@ class AbstractionViewController: UIPageViewController {
     }
     */
 
-}
+
